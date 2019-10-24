359740cf666b8aec33ca4fe502b9605ae6518c30
ioncreature <it.is.amarenin@gmail.com>
2019-10-24 17:46:02 +0300

    login action refine

--- src/services/auth/routes/login.action.js
+++ src/services/auth/routes/login.action.js
@@ -13,18 +13,17 @@ exports.fn = ({actions: {user}, singletons: {config}}) => {
         if (!req.body.name)
             return next(BadRequest('Parameter name is required'));
 
-        singUp(req.body.name).catch(next);
-
-        async function singUp(name) {
-            const token = await user.signUp(name);
-
-            res
-                .cookie(
-                    config.common.authCookieName,
-                    token,
-                    {maxAge: config.common.sessionDurationSeconds * 1000},
-                )
-                .json({result: {token}});
-        }
+        user
+            .signUp(name)
+            .then(token => {
+                res
+                    .cookie(
+                        config.common.authCookieName,
+                        token,
+                        {maxAge: config.common.sessionDurationSeconds * 1000},
+                    )
+                    .json({result: {token}});
+            })
+            .catch(next);
     };
 };
