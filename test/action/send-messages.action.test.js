039c3181b7ae01b65ff624a2b22edef61c1172d8
ioncreature <it.is.amarenin@gmail.com>
2020-06-03 12:55:57 +0400

    test: incomplete test added

--- test/action/send-messages.action.test.js
+++ test/action/send-messages.action.test.js
@@ -4,19 +4,43 @@ const broker = require('../../src/broker');
 
 describe('Action "message.send-message"', () => {
     let sendMessage;
+    let messagePublished = false;
 
     beforeAll(async() => {
-        sendMessage = await broker.mockAction('message.sendMessage');
+        sendMessage = await broker.mockAction('message.sendMessage', {
+            plugins: {
+                publish: {
+                    start() {
+                        return () => {
+                            messagePublished = true;
+                        };
+                    },
+                },
+            },
+        });
+    });
+
+    beforeEach(() => {
+        messagePublished = false;
     });
 
     it.each([
         [undefined, undefined],
-        ['alex', undefined],
-        ['alex', 100],
+        ['Alex', undefined],
+        ['Alex', 100],
     ])(
         'should throw on invalid params, path: %p %p',
         async(from, message) => {
             await expect(sendMessage(from, message)).rejects.toThrow();
         },
     );
+
+
+    it('should send message', async() => {
+        const message = await sendMessage('Alex', 'hi');
+        expect(messagePublished).toBe(true);
+        expect(message.from).toEqual('Alex');
+        expect(message.text).toEqual('hi');
+        expect(typeof message.at).toBe('number');
+    });
 });
